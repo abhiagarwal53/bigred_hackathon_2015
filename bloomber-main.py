2445d7dd261486d47f33a09a0449a370b6be3a59
girikuncoro <girikuncoro@gmail.com>
2015-09-21 10:09:30 -0400

    Fixed world map coloring issue

--- bloomber-main.py
+++ bloomber-main.py
@@ -135,5 +135,5 @@ def get_suggestions(word):
 
 port = int(os.environ.get('PORT', 5000)) 
 if __name__ == "__main__":
-    app.run(debug=True,port=port,host='0.0.0.0')
+    app.run(debug=True,port=port)
 
